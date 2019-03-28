dae87f183cd5492c2d7e19adf99475f1de30bed3
Adelinestack <adeline.colombo@stackerine.com>
2019-03-28 15:03:16 +0100

    Tests

--- client/src/utils/messages.test.js
+++ client/src/utils/messages.test.js
@@ -2,13 +2,13 @@ import { addNewMessage } from './messages';
 
 const newMessage = ['Salut'];
 
-test(`addNewMessage with new message 'Salut' and  prev state with prev messages, return a new array with prev messages and new message`, () => {
+test(`should return the message list with the added message`, () => {
   const prevState = { messages: [] };
   const newState = { messages: ['Salut'] };
   expect(addNewMessage(newMessage)(prevState)).toEqual(newState);
 });
 
-test(`addNewMessage with new message 'Salut' and  prev state with 10 prev messages, return a new array with prev messages minus the first message and new message`, () => {
+test(`should limit message list size to 10`, () => {
   const prevState = {
     messages: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
   };
